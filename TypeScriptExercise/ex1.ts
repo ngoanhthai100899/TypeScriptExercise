adf32326135e018f312884fa00407a0fd8ed198b
ngoanhthai100899 <142786189+ngoanhthai100899@users.noreply.github.com>
2023-12-11 10:25:02 +0700

    Add files via upload

---
+++ TypeScriptExercise/ex1.ts
@@ -0,0 +1,8 @@
+let number = 2;
+function checkEvenOrOddNumber(number: number): string {
+    if (number % 2 === 0) {
+        return "i is an even number";
+    } else {
+        return "i is an odd number";
+    }
+}
---
+++ TypeScriptExercise/ex3.ts
@@ -0,0 +1,7 @@
+function numberDividedBy3() {
+    for (let i = 1; i <= 50; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
